4f60a32c25a0c18618b977b10f8b41984cbe7d9d
Huzaifa Hassan <huzaifahassan507@gmail.com>
2025-01-31 09:28:22 -0800

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,5 @@
+document.getElementById('contactForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Thank you for contacting us! We will get back to you soon.');
+    // You can add more functionality here, like sending the form data to a server.
+});
